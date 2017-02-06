99c07d004ca19443dd7dc148260f0aadecc9834d
REMAKI <remakikamel@hotmail.fr>
2017-02-06 11:54:46 +0100

    structure du projet

--- src/main/java/org/lpro/entity/Accreditation.java
+++ src/main/java/org/lpro/entity/Accreditation.java
@@ -4,19 +4,11 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class Accreditation {
-    public Commande commande;
-    public Sandwich sandwich;
+    
      public Accreditation() {
     }
 
-    public Commande getCommande() {
-        return commande;
-    }
-
-    public void setCommande(Commande commande) {
-        this.commande = commande;
-    }
-
+  
    
     
     
