a87263c4512cbd7293c60f45f39c0f6f32e32100
REMAKI <remakikamel@hotmail.fr>
2017-02-06 15:26:31 +0100

    ajout d'entités

---
+++ src/main/java/org/lpro/entity/Partie.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.lpro.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author remaki
+ */
+@Entity
+public class Partie implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id //clé
+    private String id;
+    
+    
+    
+    
+    
+    
+    
+}
