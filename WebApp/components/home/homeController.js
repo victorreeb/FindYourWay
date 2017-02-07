ae9a0524703ad30130e33f9d520324e0eb592aae
reeb5u <reeb.vi@gmail.com>
2017-02-07 14:59:24 +0100

    homepage, homecontroller, ajout leaflet

--- WebApp/components/home/homeController.js
+++ WebApp/components/home/homeController.js
@@ -5,15 +5,20 @@
         .module('app')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['UserService', '$rootScope'];
+    HomeController.$inject = ['$rootScope'];
 
-    function HomeController(UserService, $rootScope) {
+    function HomeController($rootScope) {
         var vm = this;
-        vm.map = '';
+        vm.initMap = (function initMap(){
+          vm.map = L.map('mapid').setView([48.866, 2.333], 5);
+          L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar'}).addTo(vm.map);
 
-        function initMap(){
-          vm.map = L.map('map').setView([48.866, 2.333], 13);
-        };
+          // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+          //   maxZoom: 18,
+          //   id: 'map.findyourway',
+          //   accessToken: 'pk.eyJ1IjoicmVlYjV1IiwiYSI6ImNpeXZqOWo1NTAwMjcycXFhdHZnZ3c5OHYifQ.rNgjQOnrBA0uoasccb5HkA'
+          // }).addTo(vm.map);
+        })();
 
     }
 
