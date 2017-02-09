7a22e8cc95110ab96d176c0b05181f62af58acd8
ziko <realandraja@gmail.com>
2017-02-09 17:23:22 +0100

    Ajout du controleur et de la vue de l'inscription d'un admin

---
+++ WebApp/components/register/registerController.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('RegisterController', RegisterController);
+
+    RegisterController.$inject = ['$scope','UserService', '$location', '$rootScope', 'FlashService'];
+    function RegisterController($scope,UserService, $location, $rootScope, FlashService) {
+       
+        var vm = this;
+
+        vm.register = register;
+
+        function register() {
+            vm.dataLoading = true;
+            UserService.Create(vm.user)
+                .then(function (response) {
+
+                    if (response.success) {
+                        FlashService.Success('Registration successful',true);
+                        vm.dataLoading = false;
+                        $rootScope.$broadcast(AUTH_EVENTS.loginSuccess)
+                        $location.path('/login');
+                    } else {
+                        FlashService.Error(response.message);
+                        $rootScope.$broadcast(AUTH_EVENTS.loginSuccess)
+                        vm.dataLoading = false;
+                    }
+                });
+        }
+    }
+
+})();
